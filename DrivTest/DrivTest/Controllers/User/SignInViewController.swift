84a9f10848064a4fa6ad44716ae35adcb73786d9
MinhTri3013 <minhtri3013@gmail.com>
2021-01-21 05:38:39 -0800

    sign in, sign up

--- DrivTest/DrivTest/Controllers/User/SignInViewController.swift
+++ DrivTest/DrivTest/Controllers/User/SignInViewController.swift
@@ -5,12 +5,22 @@
 //  Created by HUYTU on 12/15/20.
 //  Copyright © 2020 18120200_18120250_18120254. All rights reserved.
 //
-
+import RealmSwift
 import UIKit
+
+class person: Object{
+    @objc dynamic var id: Int = -1
+    @objc dynamic var username: String = ""
+    @objc dynamic var password: String = ""
+    @objc dynamic var name: String = ""
+}
 
 class SignInViewController: UIViewController {
 
+    let realm = try! Realm()
 
+    @IBOutlet weak var userField: UITextField!
+    @IBOutlet weak var passField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +28,55 @@ class SignInViewController: UIViewController {
         
     }
     
-
-
+    @IBAction func tapSignIn(_ sender: Any) {
+        print("button sign in tapped!")
+        guard let user = userField.text, !user.isEmpty,
+            let password = passField.text, !password.isEmpty else {
+                print("missing data type")
+                showAlertLoginFailed2()
+                return
+            }
+        
+        checkLogin(_username: user, _password: password)
+    }
+    
+    @IBAction func tapBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func checkLogin(_username: String, _password: String){
+        let people = realm.objects(person.self)
+        for person in people{
+            if(_username == person.username && _password == person.password){
+                print("You have sign in")
+                
+                let vc = storyboard?.instantiateViewController(identifier: "SignInSuccessViewController") as! SignInSuccessViewController
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true, completion: nil)
+                return
+            }
+        }
+        print("login failed!")
+        showAlertLoginFailed()
+    }
+    
+    func showAlertLoginFailed(){
+        let alert = UIAlertController(title: "Đăng nhập thất bại!", message: "Tên đăng hoặc mật khẩu sai.", preferredStyle: .alert)
+       
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in }
+       
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showAlertLoginFailed2(){
+        let alert = UIAlertController(title: "Đăng nhập thất bại!", message: "Tên đăng nhập hoặc mật khẩu trống.", preferredStyle: .alert)
+       
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in }
+       
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
--- DrivTest/DrivTest/Controllers/User/SignUpViewController.swift
+++ DrivTest/DrivTest/Controllers/User/SignUpViewController.swift
@@ -5,21 +5,99 @@
 //  Created by HUYTU on 12/16/20.
 //  Copyright © 2020 18120200_18120250_18120254. All rights reserved.
 //
-
+import RealmSwift
 import UIKit
 
 class SignUpViewController: UIViewController {
 
-
+    let realm = try! Realm()
     
+    @IBOutlet weak var userField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
-
+    @IBAction func tapSignUp(_ sender: Any) {
+        print("button sign up tapped!")
+        guard let user = userField.text, !user.isEmpty,
+        let password = passField.text, !password.isEmpty else {
+            print("missing data type")
+            
+            showAlertSignUpFailed()
+            return
+        }
+        
+        if checkSignup(_username: user) == false{
+            showAlertSignUpFailed()
+            return
+        }
+        
+        guard let name = nameField.text else { return }
+        addPerson(_username: user, _password: password, _name: name)
+        
+        showAlertSignUpSuccess()
+        self.dismiss(animated: true, completion: nil)
+    }
     
+    @IBAction func tapBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
-
+    func checkSignup(_username: String) -> Bool{
+        let people = realm.objects(person.self)
+        for person in people{
+            if(_username == person.username){
+                print("username already exist")
+                return false
+            }
+        }
+        return true
+    }
+    
+    func addPerson(_username: String, _password: String, _name: String){
+        var maxID = 0
+        let people = realm.objects(person.self)
+        for person in people{
+            if(maxID < person.id){
+                maxID = person.id
+            }
+        }
+        
+        let joe = person()
+        joe.id = maxID + 1
+        joe.username = _username
+        joe.password = _password
+        joe.name = _name
+        
+        realm.beginWrite()
+        realm.add(joe)
+        try! realm.commitWrite()
+    }
+    
+    func showAlertSignUpFailed(){
+        let alert = UIAlertController(title: "Đăng ký thất bại!", message: "Tên đăng nhập trùng hoặc thiếu tên đăng nhập/ mật khẩu.", preferredStyle: .alert)
+       
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in }
+       
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showAlertSignUpSuccess(){
+        let alert = UIAlertController(title: "Đăng ký thành công!", message: "Bấm ok để tiếp tục.", preferredStyle: .alert)
+       
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+       
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
